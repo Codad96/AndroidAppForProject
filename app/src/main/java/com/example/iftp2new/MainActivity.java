c0aacdf9231d6d8f83080fef7257b0e1c9451384
Codad96 <93645922+Codad96@users.noreply.github.com>
2022-11-27 23:46:49 +0200

    First commit, fetching data from JSON

---
+++ app/src/main/java/com/example/iftp2new/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.iftp2new;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+
+import com.example.iftp2new.databinding.ActivityMainBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.Buffer;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    ArrayList<String> tempList;
+    ArrayAdapter<String> listAdapter;
+    Handler mainHandler = new Handler();
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initializeTempList();
+        binding.fetchDatabtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new fetchData().start();
+            }
+        });
+    }
+
+    private void initializeTempList(){
+        tempList = new ArrayList<>();
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,tempList);
+        binding.tempList.setAdapter(listAdapter);
+
+    }
+
+    class fetchData extends  Thread {
+
+        String data = "";
+        @Override
+        public void run() {
+            mainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMessage("Fetching Data");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+                }
+            });
+
+            try {
+                URL url = new URL("https://api.npoint.io/6f7682e53dc4c51b5daa");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = bufferedReader.readLine()) != null){
+                    data = data + line;
+                }
+
+                if (!data.isEmpty()){
+                    JSONObject jsonObject = new JSONObject(data);
+                    JSONArray temps = jsonObject.getJSONArray("Temp");
+                    tempList.clear();
+
+                for (int i = 0; i < temps.length(); i++){
+                    JSONObject tempObject = temps.getJSONObject(i);
+                    String Temp = tempObject.getString("tempReading");
+                    tempList.add(Temp);
+                }
+
+
+                }
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            mainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (progressDialog.isShowing()){
+                        progressDialog.dismiss();
+                    }
+                    listAdapter.notifyDataSetChanged();
+                }
+            });
+        }
+    }
+}
